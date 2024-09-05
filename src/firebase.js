14cf3e496aee80d9cde2f9905a4c5b9603eba065
Oceanne Charavel <ocharavel@gmail.com>
2024-09-05 09:25:05 +0200

    firebase.js update

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,23 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "AUTH_DOMAIN",
-    projectId: "PROJECT_ID",
-    storageBucket: "STORAGE_BUCKET",
-    messagingSenderId: "MESSAGING_SENDER_ID",
-    appId: "APP_ID"
-  };
-  
-  const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
-  export const db = getFirestore(app);
+  apiKey: "AIzaSyCmpotvYVHLxJmkaomZoJAn7Sx2E_8Qaac",
+  authDomain: "projet-final-ajc.firebaseapp.com",
+  projectId: "projet-final-ajc",
+  storageBucket: "projet-final-ajc.appspot.com",
+  messagingSenderId: "1076149792125",
+  appId: "1:1076149792125:web:9ba8c2096567a094703014"
+};
+
+// Initialiser Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialiser Firestore et Auth
+const db = getFirestore(app);
+console.log(db);
+
+const auth = getAuth(app);
+
+export { db, auth };

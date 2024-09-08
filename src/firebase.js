058102c8408fddfdc56e0f503717a2e4687c10ad
PLWilliam <tgp.william@gmail.com>
2024-09-08 17:03:40 +0200

    trying to fix quota

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,18 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmpotvYVHLxJmkaomZoJAn7Sx2E_8Qaac",
-  authDomain: "projet-final-ajc.firebaseapp.com",
-  projectId: "projet-final-ajc",
-  storageBucket: "projet-final-ajc.appspot.com",
-  messagingSenderId: "1076149792125",
-  appId: "1:1076149792125:web:9ba8c2096567a094703014"
+  // apiKey: "AIzaSyCmpotvYVHLxJmkaomZoJAn7Sx2E_8Qaac",
+  // authDomain: "projet-final-ajc.firebaseapp.com",
+  // projectId: "projet-final-ajc",
+  // storageBucket: "projet-final-ajc.appspot.com",
+  // messagingSenderId: "1076149792125",
+  // appId: "1:1076149792125:web:9ba8c2096567a094703014"
+  apiKey: "AIzaSyD4CG0phJ0hheY47-vkKCHSgZMXgKTooKk",
+  authDomain: "projet-final-ajc-2.firebaseapp.com",
+  projectId: "projet-final-ajc-2",
+  storageBucket: "projet-final-ajc-2.appspot.com",
+  messagingSenderId: "1088748171230",
+  appId: "1:1088748171230:web:28be227b73d4eeb23db74f"
 };
 
 // Initialiser Firebase

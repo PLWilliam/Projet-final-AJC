463a032dea2d815095bb147aa5fa0b09fd0ce472
Oceanne Charavel <ocharavel@gmail.com>
2024-09-09 16:36:17 +0200

    css card ok plus modif css filtre, les deux sont finis

--- src/components/productsPages/ProductCard.js
+++ src/components/productsPages/ProductCard.js
@@ -1,30 +1,31 @@
-import React, {useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ButtonAddDel } from '../component.js'
+import { ButtonAddDel } from '../component.js';
+import './ProductCard.css';
 
 
-const ProductCard = (data) => {
-    const [product,setProduct] = useState('')
+const ProductCard = ({ value }) => {
+    const [product, setProduct] = useState('');
 
-    useEffect(()=>{
-        setProduct(data.value)
-    },[])
+    useEffect(() => {
+        setProduct(value);
+    }, [value]);
 
-    
-  return (
-    <div className="featured-product">
-        {product.best_seller && <span className="best-seller-badge">Meilleur vendeur</span>}
-        <Link to={`/products/${product.id}`}>
-            <img src={product.poster} alt={product.name} className="product-image" />
-            <h3>{product.name}</h3>
-        </Link>
-        <p>{product.overview}</p>
-        <p>Price: {product.price} €</p>
-        <p>Rating: {product.rating} / 5</p>
-        {product.id}
-        <ButtonAddDel product={product}/>
-    </div>
-  )
+    return (
+        <div className="featured-product">
+            {product.best_seller && <span className="best-seller-badge">Meilleur vendeur</span>}
+            <Link to={`/products/${product.id}`}>
+                <img src={product.poster} alt={product.name} className="product-image" />
+                <h3>{product.name}</h3>
+            </Link>
+            <p>{product.overview}</p>
+            <p>Price: {product.price} €</p>
+            <p>Rating: {product.rating} / 5</p>
+            <div className="button-container">
+                <ButtonAddDel product={product} />
+            </div>
+        </div>
+    );
 }
 
-export default ProductCard
+export default ProductCard;
